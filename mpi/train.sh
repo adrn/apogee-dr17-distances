083766b5f9fdf5ad90cab74953c0eaead9a27674
Adrian Price-Whelan <adrian.prw@gmail.com>
2021-05-10 10:14:33 -0400

    rename

--- mpi/train.sh
+++ mpi/train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH -J joaquin
-#SBATCH -o logs/joaquin.o%j
-#SBATCH -e logs/joaquin.e%j
-#SBATCH -N 1
+#SBATCH -J joaquin-train
+#SBATCH -o logs/train.o%j
+#SBATCH -e logs/train.e%j
+#SBATCH -N 4
 #SBATCH --ntasks-per-node=64
 #SBATCH -t 12:00:00
 #SBATCH -p cca
@@ -16,7 +16,7 @@ cd /mnt/ceph/users/apricewhelan/projects/apogee-dr17-distances
 date
 
 mpirun python3 -m mpi4py.run -rc thread_level='funneled' \
-$CONDA_PREFIX/bin/joaquin run -c config.yml -v --mpi
+$CONDA_PREFIX/bin/joaquin train -c config.yml -v --mpi
 
 date
 
